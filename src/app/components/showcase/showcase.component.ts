b5ff396aad688cb4869255c04c98ac9e4515f0bc
amitshaiwale <amitshaiwale20@gmail.com>
2021-07-02 15:40:34 +0530

    prototype and font family settled

--- src/app/components/showcase/showcase.component.ts
+++ src/app/components/showcase/showcase.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-showcase',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showcase.component.scss']
 })
 export class ShowcaseComponent implements OnInit {
+
+  @Input() imgrSrc = '';
 
   constructor() { }
 
